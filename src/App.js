69c214dcdc2884ff8f852dd0b4ae0cbf0b839314
vayne <miguel.osorio19@yahoo.com>
2022-04-11 18:09:59 -0500

    testing rendering for components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Nav />
+      <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        Copyright © Active-ATX | All Rights Reserved 2022.
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
