dc961d8059ca2e2e42d84234649425821b5d38db
vayne <miguel.osorio19@yahoo.com>
2022-04-12 19:36:21 -0500

    adding images in assets folder, testing Navbar component rendering

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar, Button, NavDropdown } from 'react-bootstrap';
+import logo from '../../assets/images/Logo2.png'
 
 function Navigation() {
   return (
     <div>
+      <div>
+        <div>
+          <div>
+            <img src={logo} alt="volleball"></img>
+            <p>League Dates - March 21-27 Week 1 to May 8-15 Week 8</p>
+            <p>CTX Feildhouse Buda Tx Sand Volleyball Courts</p>
+          </div>
+        </div>
+      </div>
       <Navbar bg="charcoal" variant="dark" expand="sm" collapseOnSelect>
         <Navbar.Brand>ActiveATX</Navbar.Brand>
         <Navbar.Toggle />
@@ -20,7 +30,7 @@ function Navigation() {
               <Button variant="warning">Tournament Registration</Button>
             </Nav.Link>
             <NavDropdown>
-              <NavDropdown.Item href="/">Home</NavDropdown.Item>
+              <NavDropdown.Item href="/">Home</NavDropdown.Item> 
               <NavDropdown.Item href="/">Leagues</NavDropdown.Item>
               <NavDropdown.Item href="/">Tournament</NavDropdown.Item>
               <NavDropdown.Item href="/">Open Play</NavDropdown.Item>
