f952bba31c80e9edc3bed9390c4b1b486eaba7ea
vayne <miguel.osorio19@yahoo.com>
2022-04-14 17:41:05 -0500

    centered carousel for the meantime

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,49 +1,44 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import img1 from '../../assets/images/s1.jpg'
-import img2 from '../../assets/images/s2.jpg'
-import img3 from '../../assets/images/s3.jpg'
+import img1 from '../../assets/images/IMG_2483.jpg';
+import img2 from '../../assets/images/IMG_2444.jpg';
+import img3 from '../../assets/images/IMG_2447.jpg';
 
 function Home() {
   return (
-    <div className='carouselContainer'>
-      <Carousel variant="dark">
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={img1}
-            alt="First slide"
-          />
-          {/* <Carousel.Caption>
-            <h5>Volleyball</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={img2}
-            alt="Second slide"
-          />
-          {/* <Carousel.Caption>
-            <h5>Volleyball</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption> */}
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={img3}
-            alt="Third slide"
-          />
-          {/* <Carousel.Caption>
-            <h5>Volleyball</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption> */}
-        </Carousel.Item>
-      </Carousel>
+  
+    <div className="carouselContainer">
+      <div className="innerCarouselContainer">
+        <Carousel variant="dark">
+          <Carousel.Item>
+            <img className="d-block w-100" src={img1} alt="First slide" />
+            <Carousel.Caption>
+              <h5>LET'S PLAY!</h5>
+              <p>How about signing up?</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={img2} alt="Second slide" />
+            <Carousel.Caption>
+              <h5>NEED A TEAM?</h5>
+              <p>
+                You can also sign up as an individual and we can put you on a
+                team
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={img3} alt="Third slide" />
+            <Carousel.Caption>
+              <h5>YOU CAN BE A FREE AGENT</h5>
+              <p>
+                The registration link will be posted when registration opens for
+                each league.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </div>
   );
 }
