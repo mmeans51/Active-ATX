7fe02487cd15f0f4a2406738f0e58b1cc4f13ede
vayne <miguel.osorio19@yahoo.com>
2022-04-14 13:49:23 -0500

    reset nav elements, removed components from react

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,7 +6,7 @@ import img3 from '../../assets/images/s3.jpg'
 
 function Home() {
   return (
-    <div>
+    <div className='carouselContainer'>
       <Carousel variant="dark">
         <Carousel.Item>
           <img
@@ -14,10 +14,10 @@ function Home() {
             src={img1}
             alt="First slide"
           />
-          <Carousel.Caption>
+          {/* <Carousel.Caption>
             <h5>Volleyball</h5>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+          </Carousel.Caption> */}
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -25,10 +25,10 @@ function Home() {
             src={img2}
             alt="Second slide"
           />
-          <Carousel.Caption>
+          {/* <Carousel.Caption>
             <h5>Volleyball</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+          </Carousel.Caption> */}
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -36,12 +36,12 @@ function Home() {
             src={img3}
             alt="Third slide"
           />
-          <Carousel.Caption>
+          {/* <Carousel.Caption>
             <h5>Volleyball</h5>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
-          </Carousel.Caption>
+          </Carousel.Caption> */}
         </Carousel.Item>
       </Carousel>
     </div>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav, Navbar, Button, NavDropdown } from 'react-bootstrap';
 import logo from '../../assets/images/Logo2.png';
 
 function Navigation() {
   return (
-    <div>
-      <div className="navContainer">
-        <div className="logoContainer">
-          <div className='nav-img'>
-            <img src={logo} alt="volleyball"></img>
+    <div className='Nav'>
+      <div className="jumbotron">
+        <div>
+          <div>
+            <img className='logo' src={logo} width="150" alt='volleyball'></img>
           </div>
           <div className='nav-p'>
             <p>League Dates - March 21-27 Week 1 to May 8-15 Week 8</p>
@@ -17,10 +16,15 @@ function Navigation() {
           </div>
         </div>
       </div>
-      <Navbar bg="charcoal" variant="dark" expand="sm" collapseOnSelect>
-        
- 
-      </Navbar>
+      <div className='linkContainer'>
+        <div className='navlinks'>
+          <a href='/'>Home</a>
+          <a href='/'>Leagues</a>
+          <a href='/'>Tournament</a>
+          <a href='/'>Schedule</a>
+          <a href='/'>Rules</a>
+        </div>
+      </div>
     </div>
   );
 }
