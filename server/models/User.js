478ffdb9ae3a38ab113392684b3e2667e06d64fd
michael Means <colemeans51@gmail.com>
2022-04-20 15:44:43 -0500

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -31,11 +31,7 @@ const userSchema = new Schema(
 );
 
 // hash user password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
+
 
   next();
 });
