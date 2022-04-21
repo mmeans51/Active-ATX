5e9c18b54bc4a49056706e395211b73e66024fa8
Marcus Morua <91860506+mmorua@users.noreply.github.com>
2022-04-21 00:27:26 -0500

    feat: Add Signup create and findOneAndUpdate methods to signup-controller.js

--- controllers/signup-controller.js
+++ controllers/signup-controller.js
@@ -1,3 +1,4 @@
+const res = require('express/lib/response');
 const { Signup } = require('../models/Signup')
 
 const signupController = {
@@ -23,6 +24,21 @@ const signupController = {
             res.status(400).json(err);
           });
       },
-    }
+      createSignup({ body }, res) {
+          Signup.create(body)
+          .then(dbSignupData => res.json(dbSignupData))
+          .catch(err => res.status(400).json(err));
+      },
+      updateSignup({ params, body }, res) {
+          Signup.findOneAndUpdate({ _id: params.id }, body, { new: true })
+          .then(dbSignupData => {
+              if(!dbSignupData) {
+                  res.status(404).json({ message: 'No signup found with this id!' });
+                  return;
+              }
+              res.json(dbSignupData);
+          })
+          .catch(err => res.status(400).json(err));
+    },
 
 module.exports = signupController;
