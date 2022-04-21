c933f547a8c534c44a1b6705af3c540909833c33
Marcus Morua <91860506+mmorua@users.noreply.github.com>
2022-04-21 00:03:04 -0500

    feat: Add Signup findAll and findOne methods to signup-controller.js

--- controllers/signup-controller.js
+++ controllers/signup-controller.js
@@ -1,7 +1,28 @@
 const { Signup } = require('../models/Signup')
 
 const signupController = {
-    
-};
+    getAllSignup(req, res) {
+        Signup.find({})
+        .then(dbSignupData => res.json(dbSignupData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    getSignupById({ params }, res) {
+        Signup.findOne({ _id: params.id })
+        .then(dbSignupData => {
+            if (!dbSignupData) {
+                res.status(404).json({ message: 'No signup found with this id!' });
+                return;
+            }
+            res.json(dbSignupData);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
+    }
 
 module.exports = signupController;
