2f1b1c34ec5eeba420ad939a095f2c0ea3738298
vayne <miguel.osorio19@yahoo.com>
2022-04-21 11:33:47 -0500

    initializing the contact form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 function Contact() {
-
   return (
     <div className="mainContact">
-      <p>Let's get in touch.</p>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Subject" />
-        <input type="text" placeholder="Email" />
-        <textarea rows="5" placeholder="Message" />
-        <button>Submit</button>
-      </form>
+      <div className='contactForm'>
+        <p>Let's get in touch.</p>
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Subject" />
+          <input type="text" placeholder="Email" />
+          <textarea rows="5" placeholder="Message" />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
