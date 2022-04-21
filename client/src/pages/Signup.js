b80c89ac298b68a7b15dadb17d1cc0c317eea803
vayne <miguel.osorio19@yahoo.com>
2022-04-21 12:59:41 -0500

    setting up the sign up page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function Signup() {
   return (
-    <div className='mainSignup'>
-      Sign Up
+    <div className="mainSignup">
+      <div>
+        <div>
+          <h4>Sign Up</h4>
+          <div>
+            <form>
+              <input />
+              <input />
+              <input />
+              <button>
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
