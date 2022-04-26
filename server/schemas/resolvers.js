842ebec2570513520fa0d9f1021ee9d16e055e02
vayne <miguel.osorio19@yahoo.com>
2022-04-26 14:28:07 -0500

    updated typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,29 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello world!';
-    }
-  }
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // get all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // get a user by name
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+  },
 };
 
 module.exports = resolvers;
